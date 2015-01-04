377384c99d251422d686a877e4317fa288e7aa22
Joe Walnes <joe@walnes.com>
2015-01-04 08:47:13 -0600

    Simplified Swift code

--- NovaHelloWorldSwift/NovaHelloWorldSwift/AppDelegate.swift
+++ NovaHelloWorldSwift/NovaHelloWorldSwift/AppDelegate.swift
@@ -14,8 +14,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: NSObject, UIApplicationDelegate, NVFlashServiceDelegate {
 
-    var flashService: NVFlashService?
-    var timer: NSTimer?
+    var flashService: NVFlashService!
+    var timer: NSTimer!
 
     func applicationDidFinishLaunching(application: UIApplication) {
         NSLog("init")
@@ -24,13 +24,13 @@ class AppDelegate: NSObject, UIApplicationDelegate, NVFlashServiceDelegate {
         flashService = NVFlashService()
         
         // Optional: receive callbacks when flashes come into range or are connected/disconnected
-        flashService!.delegate = self
+        flashService.delegate = self
         
         // Auto connect to closest flash
-        flashService!.autoConnect = true
+        flashService.autoConnect = true
         
         // Uncomment to auto connect to more than one flash
-        // flashService!.autoConnectMaxFlashes = 4
+        // flashService.autoConnectMaxFlashes = 4
         
         // Set a timer to toggle flash every second.
         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self,
@@ -41,14 +41,14 @@ class AppDelegate: NSObject, UIApplicationDelegate, NVFlashServiceDelegate {
     func applicationDidBecomeActive(application: UIApplication) {
         NSLog("enable")
 
-        flashService!.enable()
+        flashService.enable()
     }
     
     // When app is not active, shut down bluetooth to conserve battery life
     func applicationWillResignActive(application: UIApplication) {
         NSLog("disable")
 
-        flashService!.disable()
+        flashService.disable()
     }
 
     // Called every second by timer
@@ -60,7 +60,7 @@ class AppDelegate: NSObject, UIApplicationDelegate, NVFlashServiceDelegate {
         
         // loop through all connected flashes
         // typically just 1, but change autoConnectMaxFlashes above for more
-        for flash in flashService!.connectedFlashes as [NVFlash] {
+        for flash in flashService.connectedFlashes as [NVFlash] {
             
             // toggle flash
             if (!flash.lit) {
